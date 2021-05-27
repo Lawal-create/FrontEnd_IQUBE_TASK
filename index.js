06acd514aa436b9575b3fd1b936c6e5c5ce959ea
Lawal Hakeem <lawizyhal@gmail.com>
2021-05-27 10:44:02 +0200

    Changed the font for the dropdown

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+
 let texts=["I have a question about your service","I have a problem with the API","I have an issue with the product that needs to be resolves urgently","I have a minor problem","My inquiry isn't amongst the ones mentioned above"];
 
-
+//loads the values into the dropdown menu as a page
 window.onload= function(){
     let x=document.getElementById('dropdown')
 
@@ -17,15 +18,14 @@ window.onload= function(){
     x.appendChild(ul);
 }
 
-
-
+//sets the values 
 function setValue(index) {
     let i=parseInt(index)
     let x=document.getElementById("dropdown_text")
     x.innerHTML=texts[i]
     
 }
-
+//shows and hides the dropdown item when clicked
 function showDropdown() {
     let x=document.getElementById("dropdown")
     if(x.style.display==="none"){
